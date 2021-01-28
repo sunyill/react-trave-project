55f86835495a5c112f5fa09b03296acd557bd95e
azan <sunyill0707@163.com>
2021-01-28 12:04:33 +0800

    使用link, to 属性进行路由跳转

--- src/components/hotTourist/ProductImageInfo.tsx
+++ src/components/hotTourist/ProductImageInfo.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { Image, Typography } from "antd";
-import  {withRouter, RouteComponentProps} from "react-router-dom"
+import  {withRouter, RouteComponentProps,Link} from "react-router-dom"
 
 interface PropsType extends RouteComponentProps{
   id: string | number;
@@ -24,7 +24,7 @@ const ProductImageInfoComponent: React.FC<PropsType> = ({
   console.log(location);
   console.log(match);
   return (
-    <div onClick = {()=>history.push(`/detail/${id}`)}>
+    <Link to={`detail/${id}`}>
       {size === "large" ? (
         <Image src={imageSrc} height={285} width={490}></Image>
       ) : (
@@ -35,7 +35,7 @@ const ProductImageInfoComponent: React.FC<PropsType> = ({
 
         <Typography.Text type="warning">¥ {price} 元起</Typography.Text>
       </div>
-    </div>
+    </Link>
   );
 };
 
