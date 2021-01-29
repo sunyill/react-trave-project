6f5fdf2609519b8126dd7991db3e532ebe1d820c
azan <sunyill0707@163.com>
2021-01-29 12:57:20 +0800

    axios 获取数据, 使用SPin 处理悬空数据

--- src/index.tsx
+++ src/index.tsx
@@ -4,15 +4,17 @@ import "./index.css";
 import App from "./App";
 import "antd/dist/antd.css";
 import './i18n/configs'
-import {Provider} from "react-redux"
+import { Provider } from "react-redux"
 import store from "./redux/store";
+import axios from 'axios'
+axios.defaults.headers['x-icode'] = 'FB80558A73FA658E';
 
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
-    <App />
+      <App />
     </Provider>
-    
+
   </React.StrictMode>,
   document.getElementById("root")
 );
--- src/pages/home/HomePage.tsx
+++ src/pages/home/HomePage.tsx
@@ -7,20 +7,67 @@ import {
   HotTourist,
   Company
 } from "../../components";
-import { Row, Col, Typography } from "antd";
-import {
-  productList1,
-  productList2,
-  productList3
-} from "../../mock/hotContent";
+import { Row, Col, Typography, Spin } from "antd";
 import image1 from "../../assets/hot/sider_2019_02-04-2.png";
 import image2 from "../../assets/hot/sider_2019_02-04.png";
 import image3 from "../../assets/hot/sider_2019_12-09.png";
 import styles from "./HomePage.module.css";
-import {withTranslation,WithTranslation} from 'react-i18next'
- class HomePageComponent extends React.Component<WithTranslation> {
+import { withTranslation, WithTranslation } from 'react-i18next'
+import axios from 'axios'
+
+interface State {
+  productList: any[],
+  loading: boolean,
+  error: string | null
+}
+class HomePageComponent extends React.Component<WithTranslation, State> {
+  constructor(props) {
+    super(props)
+    this.state = {
+      productList: [],
+      loading: true,
+      error: null
+    }
+  }
+
+  async componentDidMount() {
+    try {
+      const { data } = await axios.get("http://123.56.149.216:8080/api/productCollections")
+      this.setState({
+        loading: false,
+        error: null,
+        productList: data
+      })
+    } catch (error) {
+      this.setState({
+        loading: false,
+        error: error.message
+      })
+    }
+
+  }
+
   render() {
-    const {t} = this.props
+    const { t } = this.props
+    const { productList, loading, error } = this.state
+
+    if (loading) {
+      return (
+        <Spin
+          size="large"
+          style={{
+            marginTop: 200,
+            marginBottom: 200,
+            marginLeft: "auto",
+            marginRight: "auto",
+            width: "100%",
+          }}
+        />
+      );
+    }
+    if (error) {
+      return <div>网站出错：{error}</div>;
+    }
     return (
       <>
         <Header />
@@ -41,32 +88,33 @@ import {withTranslation,WithTranslation} from 'react-i18next'
               </Typography.Title>
             }
             sideImage={image1}
-            products={productList1}
+
+            products={productList[0].touristRoutes}
           ></HotTourist>
           <HotTourist
             title={
               <Typography.Title level={3} type="success">
-                 {t("home_page.new_arrival")}
+                {t("home_page.new_arrival")}
               </Typography.Title>
             }
             sideImage={image2}
-            products={productList2}
+            products={productList[1].touristRoutes}
           ></HotTourist>
           <HotTourist
             title={
               <Typography.Title level={3} type="warning">
-                 {t("home_page.domestic_travel")}
+                {t("home_page.domestic_travel")}
               </Typography.Title>
             }
             sideImage={image3}
-            products={productList3}
-          />
+            products={productList[2].touristRoutes}
+          ></HotTourist>
           <Company />
         </div>
-        <Footer/>
+        <Footer />
       </>
     );
   }
 }
 
-export const HomePage = withTranslation()(HomePageComponent)
+export const HomePage = withTranslation()(HomePageComponent) 
